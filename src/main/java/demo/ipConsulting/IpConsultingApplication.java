36bbe4ca1d43cfa5ffb3bb78ef4d6050d384420e
juan-para <juanpara86@gmail.com>
2021-04-17 18:30:29 -0300

    first commit

---
+++ src/main/java/demo/ipConsulting/IpConsultingApplication.java
@@ -0,0 +1,13 @@
+package demo.ipConsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/ipConsulting/IpConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ipConsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
